7fb833bdc066dd79818b746f11b172cfdefbce5f
alex <alexanderalexeyo@gmail.com>
2025-05-28 20:59:41 +0530

    adding styles to pages

--- base/views.py
+++ base/views.py
@@ -119,6 +119,8 @@ def createRoom(request):
     if request.method == 'POST':
         form = RoomForm(request.POST)
         if form.is_valid():
+            room = form.save(commit=False)
+            room.host = request.user
             form.save()
             return redirect('home')
 
