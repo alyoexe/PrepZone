ac822e27ea563baf0b8e42f006adecc6b7175ab3
alex <alexanderalexeyo@gmail.com>
2025-06-02 12:56:52 +0530

    working

--- studybud/settings.py
+++ studybud/settings.py
@@ -124,12 +124,12 @@ USE_TZ = True
 STATIC_URL = "/static/"
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
-MEDIA_URL = '/images/'
+MEDIA_URL = '/media/'
 STATICFILES_DIRS = [
     BASE_DIR / "static"
 ]
 
-MEDIA_ROOT = BASE_DIR / 'static/images'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
